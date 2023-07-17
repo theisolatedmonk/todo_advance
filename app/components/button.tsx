54421898139be06bbe03eaf685ed62b2d57384fd
theisolatedmonk <96rahulseth@gmail.com>
2023-07-17 23:16:34 +0530

    .

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function DropdownComponent() {
     return (
         <div className="relative w-[70%] lg:max-w-sm">
-            <select className="w-full p-2.5 text-gray-500 bg-white border  shadow-sm outline-none appearance-none focus:border-indigo-600">
+            <select className="w-full p-2.5 text-black bg-white border  shadow-sm outline-none  focus:border-black">
             {/* <option>Select Statuse</option> */}
                 <option>TODO</option>
                 <option>In Progress</option>
