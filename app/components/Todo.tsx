b5c6ddc8460d8b97a10bde8a3cad3bf648ef31fa
theisolatedmonk <96rahulseth@gmail.com>
2023-07-17 23:16:24 +0530

    DELETE BUTTON ADDED

---
+++ app/components/Todo.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React, { useState } from 'react'
+import DropdownComponent from './button'
+
+type Props = {}
+
+export default function Todo({}: Props) {
+
+    const [activity, setActivity] = useState('')
+    const [listData, setlistData] = useState([])
+
+    // function addActivity(){
+    //     setlistData((listData)=>{
+    //         const updatedlist= [...listData, activity]
+    //         setActivity('')
+    //         return updatedlist
+    //     })
+    // }
+
+
+  return (
+    <div className="flex flex-col  w-full gap-8">  <div className="w-full text-center text-3xl font-bold ">Todo APP</div>
+
+    <div className="flex  w-full bg-white justify-between rounded-lg overflow-hidden p-2">
+     <input className='w-[86%] p-2 outline-none text-black' type="text" value={activity} onChange={(e)=>setActivity(e.target.value)} />
+     <button className='
+     p-2 rounded-md bg-black '>Create TODO</button>
+    </div>
+    
+    <div className="flex  w-full bg-white justify-between rounded-lg overflow-hidden p-2 gap-2">
+     <div className='w-[70%] p-2 outline-none text-black'  > </div>
+    <div className="flex w-[40%] gap-1"> <DropdownComponent/>
+     <button className='
+     py-2 px-4 rounded-md bg-black w-[30%] '>Edit</button>
+     <button className='
+     py-2 px-4 text-center rounded-md bg-black w-[30%] '>DELETE</button>
+     </div>
+    </div></div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,16 @@
 import Image from 'next/image'
 import DropdownComponent from './components/button'
+import Todo from './components/Todo'
+import { useState } from 'react'
 
 export default function Home() {
+
+  
+
   return (
-    <main className="flex min-h-screen flex-col items-center  gap-8 p-24 ">
+    <main className="flex min-h-screen flex-col items-center   p-24 ">
      
-     <div className="w-full text-center text-3xl font-bold ">Todo APP</div>
-
-     <div className="flex  w-full bg-white justify-between rounded-lg overflow-hidden p-2">
-      <input className='w-[86%] p-2 outline-none text-black' type="text" />
-      <button className='
-      p-2 rounded-md bg-black '>Create TODO</button>
-     </div>
-
-     <div className="flex  w-full bg-white justify-between rounded-lg overflow-hidden p-2">
-      <div className='w-[70%] p-2 outline-none text-black'  > </div>
-  <div className="flex w-[30%] gap-2"> <DropdownComponent/>
-      <button className='
-      py-2 px-4 rounded-md bg-black w-[30%] '>Edit</button></div>
-     </div>
+   <Todo/>
     </main>
   )
 }
